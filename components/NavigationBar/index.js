cbbe3c63a458fb1d2009f243145a7f15f66e684c
colnies <colin.nies6@gmail.com>
2024-02-05 16:24:44 -0500

    mobile design changes

--- components/NavigationBar/index.js
+++ components/NavigationBar/index.js
@@ -7,7 +7,7 @@ export const NavigationBar = ({selected}) => {
     return (
         <div className={styles.navbar}>
             <ul className={styles.navList}>
-                <li  className={selected === 'home' ? styles.active : styles.nav}><a href='/'>Home/</a></li>
+                <li  className={selected === 'home' ? styles.active : styles.nav}><a href='/'>Home /</a></li>
                 <li  className={selected === 'about' ? styles.active : styles.nav}><a href='/about'>About</a></li>
                 <li  className={selected === 'personal' ? styles.active : styles.nav}><a href='/personal'>Personal</a></li>
                 <li  className={selected === 'resume' ? styles.active : styles.nav}><a href='/resume'>Resume</a></li>
